e8ab517d031f35a79ba56628cfb6857776423db2
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-17 14:23:03 -0600

    initial commit

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/model'        #Model class
+
+
+get '/' do
+    "Hello, World!"
+end
