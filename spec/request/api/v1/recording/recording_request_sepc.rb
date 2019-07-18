465f0856761c5d43d43fd631ea9caef10e8868d3
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-17 18:41:50 -0600

    Adds first test

--- spec/request/api/v1/recording/recording_request_sepc.rb
+++ spec/request/api/v1/recording/recording_request_sepc.rb
@@ -1,5 +1,14 @@
 require "./spec/rails_helper"
 
 describe "Vote Requests" do
-  
+  describe "Gets ratings for a specific location" do
+
+    get '/api/v1/Recording/1/recording_rating'
+
+    expect(response).to be_successful
+
+    ratings = JSON.parse(response.body)
+
+    expect(ratings).to eq("something")
+  end
 end
