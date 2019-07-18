74d5fe368af98056b2d90155cfa8e36bb9ef0d16
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-18 17:27:41 -0600

    changes votes application to looking for single recording

--- app/controllers/votes_app.rb
+++ app/controllers/votes_app.rb
@@ -1,8 +1,8 @@
 class VotesApp < Sinatra::Base
   set :root, File.expand_path("..",__dir__)
 
-  get '/api/v1/recording/:id/recording_rating' do
-    recording = RecordingSerializer.new(Category.recording_rating)
+  get '/api/v1/recording/:id/' do
+    recording = RecordingSerializer.new(Recording.find(params[:id]))
     recording.to_json
   end
 end
