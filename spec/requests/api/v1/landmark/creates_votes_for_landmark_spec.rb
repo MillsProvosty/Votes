e11a8b39088d45d5a9f21b555d205539dba849d2
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-19 14:20:32 -0600

    Adds votes for landmarks

--- spec/requests/api/v1/landmark/creates_votes_for_landmark_spec.rb
+++ spec/requests/api/v1/landmark/creates_votes_for_landmark_spec.rb
@@ -2,10 +2,28 @@ require "./spec/spec_helper"
 
 describe "Create Landmark Vote" do
   it "Creates a vote for a landmark, with an id, and changes total score" do
-    votable_id = 25
-    votable_type = "Landmark"
-    user_token = "19384uksjehf"
-    rating = -1
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: -1, user_token: "12049oOwjhsfe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: 1, user_token: "348205whfna23afe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: 1, user_token: "348205okadefe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: -1, user_token: "84jwer2ordafe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: -1, user_token: "5482adfjq3r8fe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: 1, user_token: "1482adfjq3r8fe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: -1, user_token: "3432ih5v3r3r8fe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: 1, user_token: "12cnrufjq3r8fe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: 1, user_token: "190urke8fe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: 1, user_token: "nfje284448fe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: 1, user_token: "9283kjf8fe")
+    Vote.create(votable_id: 25, votable_type: "Landmark", rating: 1, user_token: "0i21keuhr3r8fe")
+
+    get '/api/v1/Landmark/25/score'
+
+    expect(last_response).to be_ok
+
+    ratings = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(ratings[:data][:attributes][:upvotes]).to eq(8)
+    expect(ratings[:data][:attributes][:downvotes]).to eq(4)
+    expect(ratings[:data][:attributes][:total_score]).to eq(4)
 
     post '/api/v1/Landmark/25/create_vote/19384uksjehf/-1'
 
@@ -18,5 +36,15 @@ describe "Create Landmark Vote" do
     expect(vote[:rating]).to eq(-1)
     expect(vote[:user_token]).to eq("19384uksjehf")
     expect(vote[:votable_type]).to eq("Landmark")
+
+    get '/api/v1/Landmark/25/score'
+
+    expect(last_response).to be_ok
+
+    ratings = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(ratings[:data][:attributes][:upvotes]).to eq(8)
+    expect(ratings[:data][:attributes][:downvotes]).to eq(5)
+    expect(ratings[:data][:attributes][:total_score]).to eq(3)
   end
 end
