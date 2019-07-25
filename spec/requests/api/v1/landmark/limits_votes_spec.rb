251cbb796283c3173cc20c20928ef5966b7dff9d
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-25 11:14:13 -0600

    Works for landmakrs too.

--- spec/requests/api/v1/landmark/limits_votes_spec.rb
+++ spec/requests/api/v1/landmark/limits_votes_spec.rb
@@ -3,19 +3,19 @@ require "./spec/spec_helper"
 describe "User can only vote once" do
   it "then update is called" do
 
-    Vote.create(votable_id: 22, votable_type: "landmark", rating: 1, user_token: "12345")
+    Vote.create(votable_id: 60, votable_type: "landmark", rating: 1, user_token: "12345")
 
-    Vote.create(votable_id: 22, votable_type: "landmark", rating: 1, user_token: "9834u")
-    Vote.create(votable_id: 22, votable_type: "landmark", rating: 1, user_token: "123045u")
-    Vote.create(votable_id: 22, votable_type: "landmark", rating: 1, user_token: "0923nf")
-    Vote.create(votable_id: 22, votable_type: "landmark", rating: -1, user_token: "re3nf")
-    Vote.create(votable_id: 22, votable_type: "landmark", rating: -1, user_token: "23f3nf")
+    Vote.create(votable_id: 60, votable_type: "landmark", rating: 1, user_token: "9834u")
+    Vote.create(votable_id: 60, votable_type: "landmark", rating: 1, user_token: "123045u")
+    Vote.create(votable_id: 60, votable_type: "landmark", rating: 1, user_token: "0923nf")
+    Vote.create(votable_id: 60, votable_type: "landmark", rating: -1, user_token: "re3nf")
+    Vote.create(votable_id: 60, votable_type: "landmark", rating: -1, user_token: "23f3nf")
 
     Vote.create(votable_id: 25, votable_type: "landmark", rating: 1, user_token: "12345")
     Vote.create(votable_id: 1, votable_type: "landmark", rating: 1, user_token: "12345")
     Vote.create(votable_id: 94, votable_type: "landmark", rating: -1, user_token: "12345")
 
-    get '/api/v1/landmark/22/score'
+    get '/api/v1/landmark/60/score'
 
     expect(last_response).to be_ok
 
@@ -25,10 +25,10 @@ describe "User can only vote once" do
     expect(original_vote[:data][:attributes][:downvotes]).to eq(2)
     expect(original_vote[:data][:attributes][:total_score]).to eq(2)
 
-    post '/api/v1/landmark/22/create_vote/12345/-1'
+    post '/api/v1/landmark/60/create_vote/12345/-1'
 
 
-    get '/api/v1/recording/22/score'
+    get '/api/v1/landmark/60/score'
 
     expect(last_response).to be_ok
 
