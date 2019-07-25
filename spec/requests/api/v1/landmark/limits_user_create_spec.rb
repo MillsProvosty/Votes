898bd1a0399a0fb3178e867166d18447ac5d959a
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-25 10:52:58 -0600

    makes test more explicit

--- spec/requests/api/v1/landmark/limits_user_create_spec.rb
+++ spec/requests/api/v1/landmark/limits_user_create_spec.rb
@@ -4,25 +4,38 @@ describe "User can only vote once" do
   it "then update is called" do
 
     Vote.create(votable_id: 22, votable_type: "recording", rating: 1, user_token: "12345")
+
     Vote.create(votable_id: 22, votable_type: "recording", rating: 1, user_token: "9834u")
     Vote.create(votable_id: 22, votable_type: "recording", rating: 1, user_token: "123045u")
     Vote.create(votable_id: 22, votable_type: "recording", rating: 1, user_token: "0923nf")
     Vote.create(votable_id: 22, votable_type: "recording", rating: -1, user_token: "re3nf")
     Vote.create(votable_id: 22, votable_type: "recording", rating: -1, user_token: "23f3nf")
+
     Vote.create(votable_id: 25, votable_type: "recording", rating: 1, user_token: "12345")
     Vote.create(votable_id: 1, votable_type: "recording", rating: 1, user_token: "12345")
     Vote.create(votable_id: 94, votable_type: "recording", rating: -1, user_token: "12345")
-
-    post '/api/v1/recording/22/create_vote/12345/-1'
 
     get '/api/v1/recording/22/score'
 
     expect(last_response).to be_ok
 
-    update = JSON.parse(last_response.body, symbolize_names: true)
+    original_vote = JSON.parse(last_response.body, symbolize_names: true)
 
-    expect(update[:data][:attributes][:upvotes]).to eq(3)
-    expect(update[:data][:attributes][:downvotes]).to eq(3)
-    expect(update[:data][:attributes][:total_score]).to eq(0)
+    expect(original_vote[:data][:attributes][:upvotes]).to eq(4)
+    expect(original_vote[:data][:attributes][:downvotes]).to eq(2)
+    expect(original_vote[:data][:attributes][:total_score]).to eq(2)
+
+    post '/api/v1/recording/22/create_vote/12345/-1'
+
+
+    get '/api/v1/recording/22/score'
+
+    expect(last_response).to be_ok
+
+    updated_vote = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(updated_vote[:data][:attributes][:upvotes]).to eq(3)
+    expect(updated_vote[:data][:attributes][:downvotes]).to eq(3)
+    expect(updated_vote[:data][:attributes][:total_score]).to eq(0)
   end
 end
