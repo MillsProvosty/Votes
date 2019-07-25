9cd74bc70511252a0f2c63a4b1551c741174317f
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-25 10:18:04 -0600

    Only allows a user to vote once- next vote cancels out previous vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -38,9 +38,9 @@ class Vote < ActiveRecord::Base
     if vote_id.count == 0
       Vote.create(params)
     else
-      vote_id = vote_id.first[:id]
+      id = vote_id.first[:id]
       rating = 0
-      self.update_vote(vote_id, rating)
+      self.update_vote(id, rating)
     end
   end
 end
--- spec/requests/api/v1/landmark/limits_user_create_spec.rb
+++ spec/requests/api/v1/landmark/limits_user_create_spec.rb
@@ -3,20 +3,29 @@ require "./spec/spec_helper"
 describe "User can only vote once" do
   it "then update is called" do
 
-    Vote.create(votable_id: 22, votable_type: "recording", rating: -1, user_token: "12345")
+    Vote.create(votable_id: 22, votable_type: "recording", rating: 1, user_token: "12345")
+    #user votes 1
+    Vote.create(votable_id: 22, votable_type: "recording", rating: 1, user_token: "9834u")
+    Vote.create(votable_id: 22, votable_type: "recording", rating: 1, user_token: "123045u")
+    Vote.create(votable_id: 22, votable_type: "recording", rating: 1, user_token: "0923nf")
+    Vote.create(votable_id: 22, votable_type: "recording", rating: -1, user_token: "re3nf")
+    Vote.create(votable_id: 22, votable_type: "recording", rating: -1, user_token: "23f3nf")
+    #should be 4
     Vote.create(votable_id: 25, votable_type: "recording", rating: 1, user_token: "12345")
     Vote.create(votable_id: 1, votable_type: "recording", rating: 1, user_token: "12345")
     Vote.create(votable_id: 94, votable_type: "recording", rating: -1, user_token: "12345")
 
 
-    post '/api/v1/recording/22/create_vote/12345/-1'
+    post '/api/v1/recording/22/create_vote/12345/1'
+
+    get '/api/v1/recording/22/score'
 
     expect(last_response).to be_ok
 
     update = JSON.parse(last_response.body, symbolize_names: true)
-binding.pry
-    expect(update[:data][:attributes][:upvotes]).to eq(1)
+
+    expect(update[:data][:attributes][:upvotes]).to eq(3)
     expect(update[:data][:attributes][:downvotes]).to eq(2)
-    expect(update[:data][:attributes][:total_score]).to eq(-1)
+    expect(update[:data][:attributes][:total_score]).to eq(1)
   end
 end
