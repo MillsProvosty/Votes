3d9419a53ef956ac4d9e9568324b78586be05385
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-09 17:20:38 -0600

    Adds spec for sorting votes.
    Co-authored-by: Alexandra Chakeres <alexchakeres@gmail.com>

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe Vote, type: :model do
     it {should validate_presence_of :rating }
     it {should validate_presence_of :user_token}
   end
+
+  describe 'class methods' do
+    it "sorts votable_id's by total score" do
+      vote1 = Vote.create(votable_type: "recording", votable_id: 12, rating: 1, user_token:
+      "ojset0293jhtiuwgrh")
+      vote2 = Vote.create(votable_type: "recording", votable_id: 23, rating: -1, user_token:
+      "ojset0aoer903uwgrh")
+
+      expected = {
+        12 => 1,
+        34 => 0,
+        23 => -1
+      }
+
+      expect(Vote.sort_by_rating("recording", [23, 34, 12])).to eq(expected)
+    end
+  end
 end
